7ccc62eb457940974c8e41ac0768d831dbcade02
Falex2000 <alam.lu.ar@gmail.com>
2025-11-03 22:48:21 -0600

    Implementing Save system to Settings menu

--- Robot/Assets/_Project/Scripts/UI/SettingMenu.cs
+++ Robot/Assets/_Project/Scripts/UI/SettingMenu.cs
@@ -6,18 +6,60 @@ namespace Robot
     public class SettingMenu : MonoBehaviour
     {
         [SerializeField] private Slider _musicVolumeSlider, _sfxVolumeSlider;
+        private SettingsSaveFileData _saveFileData;
 
         private void Start()
         {
-            LoadVolumeValues();
-            _sfxVolumeSlider.onValueChanged.AddListener(AudioManager.Source.SetSFXVolume);
-            _musicVolumeSlider.onValueChanged.AddListener(AudioManager.Source.SetMusicVolume);
+            LoadFromSaveSystem();
+            _sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
+            _musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
         }
 
-        private void LoadVolumeValues()
+        private void SetSFXVolume(float value)
         {
-            _sfxVolumeSlider.value = AudioManager.Source.CurrentSFXVolume;
-            _musicVolumeSlider.value = AudioManager.Source.CurrentMusicVolume;
+            _saveFileData.SoundVolume = value;
+            AudioManager.Source.SetSFXVolume(value);
+        }
+
+        private void SetMusicVolume(float value)
+        {
+            _saveFileData.MusicVolume = value;
+            AudioManager.Source.SetMusicVolume(value);
+        }
+        
+        private void InitializeSFXVolume(float value)
+        {
+            _sfxVolumeSlider.value = value;
+            AudioManager.Source.SetSFXVolume(value);
+        }
+
+        private void InitializeMusicVolume(float value)
+        {
+            _musicVolumeSlider.value = value;
+            AudioManager.Source.SetMusicVolume(value);
+        }
+        
+        public void LoadFromSaveSystem()
+        {
+            if (!_saveFileData)
+            {
+                _saveFileData = SaveSystemManager.Source.GetFileData<SettingsSaveFileData>();
+                if (!_saveFileData)
+                {
+                    _saveFileData = ScriptableObject.CreateInstance<SettingsSaveFileData>();
+                    _saveFileData.MusicVolume = 1;
+                    _saveFileData.SoundVolume = 1;
+                    SaveToSaveFile();
+                }
+            }
+
+            InitializeSFXVolume(_saveFileData.SoundVolume);
+            InitializeMusicVolume(_saveFileData.MusicVolume);
+        }
+
+        public void SaveToSaveFile()
+        {
+            SaveSystemManager.Source.SaveFileData(_saveFileData);
         }
     }
 }
---
+++ Robot/Assets/_Project/Scripts/UI/SettingsSaveFileData.cs
@@ -0,0 +1,11 @@
+using UnityEditor.Overlays;
+using UnityEngine;
+
+namespace Robot
+{
+    public class SettingsSaveFileData : FileData
+    {
+        public float MusicVolume;
+        public float SoundVolume;
+    }
+}
